e37ed554cce53bc33803093b2085ac1b63d20fdc
manjunath-grl <manjunath.grl@outlook.com>
2023-11-07 16:40:48 +0530

    Add Demo1.py file

---
+++ Sample_Pyhton_files/demo_1.py
@@ -0,0 +1,15 @@
+"""Module providing a function printing python version."""
+import sys
+
+def print_python_version():
+    print(sys.version)
+
+print("Hello, User")
+
+def squ(a):
+    return a*a
+
+provided_Num = input("Please enter any integer: ")
+
+print(squ(int(provided_Num)))
+print_python_version()
