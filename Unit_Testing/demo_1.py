fd13d1a775e66a3381fa4c114ac62c0e0b4130be
manjunath-grl <manjunath.grl@outlook.com>
2023-11-08 13:14:46 +0530

    modify Demo1.py

--- Unit_Testing/demo_1.py
+++ Unit_Testing/demo_1.py
@@ -7,14 +7,15 @@ from Sample_Python_files.demo_1 import Calculator
 
 #sys.path.insert(0, '/home/grl/Desktop/Git-Actions-Demo/Sample_Python_files')
 
-def setUp(self):
-        self.calc = Calculator()
 
 class Calculator(unittest.TestCase):
     def test_to_see_square_root_works_ok(self):
-        result = self.calc.squ(4)
+        result = self.Calculator.squ(4)
         self.assertEqual(16, result)
-        result = self.calc.squ(0)
+        result = self.Calculator.squ(0)
         self.assertEqual(0, result)
-        result = self.calc.squ(-4)
+        result = self.Calculator.squ(-4)
         self.assertEqual(16, result)
+
+if __name__=='__main__':
+	unittest.main()
