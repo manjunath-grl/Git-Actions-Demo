24ba6750189a34e452a614a55f006391a60e44f1
manjunath-grl <manjunath.grl@outlook.com>
2023-11-09 11:24:04 +0530

    Modified demo_2.py

--- unitTesting/demo_2.py
+++ unitTesting/demo_2.py
@@ -9,6 +9,8 @@ class CheckPrime(unittest.TestCase):
     def test_prime(self):
         print("Running Unit test to check value 3 is Prime or not")
         self.assertTrue(is_prime(3))
+        print("Running Unit test to check value 10 is Prime or not")
+        self.assertFalse(is_prime(10))
 
 
 if __name__ == '__main__':
