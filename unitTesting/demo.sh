0ae62c5206aca698f3afe56be42af7c80308b49c
manjunath-grl <manjunath.grl@outlook.com>
2023-11-27 13:19:49 +0530

    modified git actions

--- unitTesting/demo.sh
+++ unitTesting/demo.sh
@@ -5,24 +5,28 @@ declare -a filesChanged
 filesChanged=("samplePythonfiles/demo_1.py" "samplePythonfiles/demo_3.py" "samplePythonfiles/demo_5.py" "samplePythonfiles/demo_6.py" "samplePythonfiles/demo_2.py" )
 
 declare -a tests_list
-tests_list=("samplePythonfiles/demo_1.py" "samplePythonfiles/demo_2.py")
+tests_list=($(ls -F --file-type *.py))
 
-text="samplePythonfiles/"
+#text="samplePythonfiles/"
 
 for key in "${!tests_list[@]}" ;
 do
-   #echo "tests_list: "${tests_list[$key]} "and index:" $key
-   for value in "${!filesChanged[@]}" ;
-   do
+   echo "tests_list: "${tests_list[$key]} "and index:" $key
+   #for value in "${!filesChanged[@]}" ;
+   #do
        #echo "filesChanged: "${filesChanged[$value]} "and index:" $value
-       if [[ ${filesChanged[$value]} == ${tests_list[$key]} ]] ;
+       #if [[ ${filesChanged[$value]} == ${tests_list[$key]} ]] ;
+       if echo "${filesChanged[@]}" | grep -qw "${tests_list[$key]}";
        then
            test=${tests_list[$key]}
-           test_name="${test#*$text}"
+           #test_name="${test#*$text}"
            #echo ${test_name}
+           echo $test
            #Runs unit tests
-           python3 -m unittest ${test_name} -v
-           break
+           #python3 -m unittest ${test_name} -v
+           python3 -m unittest ${test} -v
+           #breakclear
+           
        fi
-   done
+   #done
 done
